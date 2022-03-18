feab8a5170deffdd6819b42e0e7caf626ee9e6f6
AlanMathews <mathews.alan@gmail.com>
2022-03-18 12:01:48 +0000

    wrote get result function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,47 @@ gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
 
 const choices = ['Rock', 'Paper', 'Scissors']
 let userChoice
+let computerChoice
 
-const handleClick = (e) =>{
-  userChoice = e.target.id; 
-  userChoiceDisplay.innerHTML = userChoice;
+const handleClick = (e) => {
+  userChoice = e.target.id
+  userChoiceDisplay.innerHTML = 'User Choice: ' + userChoice
   generateComputerChoice()
+  getResult()
 }
 
 const generateComputerChoice = () => {
-  const randomChoice = choices[Math.floor(Math.random() * choices.length)];  // uses math.random and math.floor to get a random number between 0 and 2, this. then selects the corresponding array choice
-  computerChoiceDisplay.innerHTML = randomChoice 
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)]
+  computerChoice = randomChoice
+  computerChoiceDisplay.innerHTML = 'Computer Choice: ' + computerChoice
 }
 
 for (let i = 0; i < choices.length; i++) {
   const button = document.createElement('button')
-  button.id = choices[i] 
+  button.id = choices[i] // you can delete this id you want to use e.target.HTML in the handleClick
   button.innerHTML = choices[i]
   button.addEventListener('click', handleClick)
   gameGrid.appendChild(button)
 }
 
+
+// Combine the two strings from the two possible choices and check them against the 9 possible scenarios, this will increase in the case of lizard and spock additions
+const getResult = () => {
+  switch (userChoice + computerChoice) {
+    case 'ScissorsPaper':
+    case 'RockScissors':
+    case 'PaperRock':
+      resultDisplay.innerHTML = "YOU WIN!" //Prints out You Win for all cases where user choice defeats computer
+      break
+    case 'PaperScissors':
+    case 'ScissorsRock':
+    case 'RockPaper':
+      resultDisplay.innerHTML = "YOU LOSE!" //Prints out You Lose for all cases where computer choice defeats computer
+      break
+    case 'PaperPaper':
+    case 'ScissorsScissors':
+    case 'RockRock':
+    resultDisplay.innerHTML = "ITS A DRAW!"// Where the answers are the same the computer displays "ITS A DRAW!"
+    break
+  }
+}
