da4263f9cfbfdb92076e00ac5e5269a901c0374b
AlanMathews <mathews.alan@gmail.com>
2022-03-18 11:11:03 +0000

    generate computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,17 @@ const gameGrid = document.getElementById('game')
 gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
 
 const choices = ['Rock', 'Paper', 'Scissors']
+let userChoice
 
-const handleClick = () =>{
-  console.log('clicked')
+const handleClick = (e) =>{
+  userChoice = e.target.id; 
+  userChoiceDisplay.innerHTML = userChoice;
+  generateComputerChoice()
+}
+
+const generateComputerChoice = () => {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)];  // uses math.random and math.floor to get a random number between 0 and 2, this. then selects the corresponding array choice
+  computerChoiceDisplay.innerHTML = randomChoice 
 }
 
 for (let i = 0; i < choices.length; i++) {
