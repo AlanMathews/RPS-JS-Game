f1206bb51b1b315696ab1481494ee640e8409694
AlanMathews <mathews.alan@gmail.com>
2022-03-18 10:50:52 +0000

    Added buttons and basic event listener in JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
+const userChoiceDisplay = document.createElement('h1')
+const computerChoiceDisplay = document.createElement('h1')
+const resultDisplay = document.createElement('h1')
+const gameGrid = document.getElementById('game')
+gameGrid.append(userChoiceDisplay, computerChoiceDisplay, resultDisplay)
 
+const choices = ['Rock', 'Paper', 'Scissors']
 
-function generateComputerChoice() {
+const handleClick = () =>{
+  console.log('clicked')
 }
 
-function whoWins() {
+for (let i = 0; i < choices.length; i++) {
+  const button = document.createElement('button')
+  button.id = choices[i] 
+  button.innerHTML = choices[i]
+  button.addEventListener('click', handleClick)
+  gameGrid.appendChild(button)
 }
 
-function incrementPlayerScore() {
-}
-
-function incrementComputerScore() {
-}
